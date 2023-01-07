b5e629920c2c44f3056308edc5ce1541c6813bd3
overdeveloped-zygote <90350841+overdeveloped-zygote@users.noreply.github.com>
2023-01-07 15:03:27 -0700

    Add files via upload

---
+++ write-images-to-dir.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Opens the inbuilt camera of laptop to capture video.
+cap = cv2.VideoCapture(0)
+i = 0
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    # This condition prevents from infinite looping 
+    # incase video ends.
+    if ret == False:
+        break
+
+    # Save Frame by Frame into disk using imwrite method
+    cv2.imwrite('Frame'+str(i)+'.jpg', frame)
+    i += 1
+
+cap.release()
