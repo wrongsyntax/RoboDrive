541d68f8c797304e75f62e2693e6ae1b7cc71eff
Uzair Tariq <uzair0213@icloud.com>
2023-01-07 20:56:08 -0700

    line

---
+++ lines.py
@@ -0,0 +1,57 @@
+import cv2 as cv
+import numpy as np
+
+import filter
+
+
+NAME = "IMG_0111"
+
+filter.create_mask(NAME)
+
+img = cv.imread(f"./masks/{NAME}_mask.jpeg")
+grayscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# cv.imwrite("gray.jpeg", grayscale)
+
+# Apply edge detection method on the image
+edges = cv.Canny(grayscale, 100, 500)
+# This returns an array of r and theta values
+lines = cv.HoughLines(edges, 1, np.pi / 180, 200)
+
+# The below for loop runs till r and theta values
+# are in the range of the 2d array
+for r_theta in lines:
+    arr = np.array(r_theta[0], dtype=np.float64)
+    r, theta = arr
+    # Stores the value of cos(theta) in a
+    a = np.cos(theta)
+
+    # Stores the value of sin(theta) in b
+    b = np.sin(theta)
+
+    # x0 stores the value rcos(theta)
+    x0 = a * r
+
+    # y0 stores the value rsin(theta)
+    y0 = b * r
+
+    # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
+    x1 = int(x0 + 5000 * (-b))
+
+    # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
+    y1 = int(y0 + 5000 * a)
+
+    # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
+    x2 = int(x0 - 5000 * (-b))
+
+    # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
+    y2 = int(y0 - 5000 * a)
+
+    # cv.line draws a line in img from the point(x1,y1) to (x2,y2).
+    # (0,0,255) denotes the colour of the line to be
+    # drawn. In this case, it is red.
+    cv.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+# All the changes made in the input image are finally
+# written on a new image houghlines.jpg
+cv.imwrite(f"./lines/{NAME}_lines.jpeg", img)
